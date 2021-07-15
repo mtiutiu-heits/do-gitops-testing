db0f70f65ad411712d9cad1077ee4a49ed6e6eb6
Tiutiu Marius Cristian <mtiutiu@Tiutius-MacBook-Pro.local>
2021-07-15 19:30:11 +0300

    added docs integration via Terraform

--- flux-cd/bootstrap/terraform/variables.tf
+++ flux-cd/bootstrap/terraform/variables.tf
@@ -1,3 +1,38 @@
+# DOKS
+variable "doks_cluster_name" {
+  type = string
+  default = "mtiutiu-testing-cluster"
+  description = "DOKS cluster name"
+}
+
+variable "doks_cluster_region" {
+  type = string
+  default = "lon1"
+  description = "DOKS cluster region"
+}
+
+variable "doks_cluster_version" {
+  type = string
+  default = "1.21.2-do.2" # Grab the latest version slug from `doctl kubernetes options versions`
+  description = "Kubernetes version provided by DOKS"
+}
+
+variable "doks_cluster_pool_size" {
+  type = string
+  default = "s-2vcpu-4gb"
+  description = "DOKS cluster node pool size"
+}
+
+variable "doks_cluster_pool_nodes" {
+  type = map
+  default = {
+    min = 3
+    max = 5
+  }
+  description = "DOKS cluster node pool limits"
+}
+
+# Github
 variable "github_owner" {
   type        = string
   description = "github owner"
@@ -9,8 +44,8 @@ variable "github_token" {
 }
 
 variable "github_ssh_pub_key" {
-  type = string
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
+  type        = string
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
   description = "github ssh public key"
 }
 
